d6113569a25f3cc1b654165ae3b1f4f2e4ac659c
Tony <ttech@gmx.de>
2023-11-30 08:22:16 +0100

    update

--- django_chat_app/chat/admin.py
+++ django_chat_app/chat/admin.py
@@ -1,3 +1,10 @@
+from .models import Message
 from django.contrib import admin
 
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('text','created_at','author','receiver')
+    list_display = ('author','text','created_at','receiver')
+    search_fields = ('text',)
+
 # Register your models here.
+admin.site.register(Message,MessageAdmin)
--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'chat/index.html')
+    return render(request, 'chat/index.html',{'username': 'Tony'})
